d9a9cb0612a29fe9a443697ad4fa7160ff435e75
Richard Herman <richard.herman@gmail.com>
2015-09-25 11:52:10 +0100

    Housekeeping

--- config/routes.js
+++ config/routes.js
@@ -25,6 +25,13 @@ function registerRoutes(app) {
         
         app.use('/' + route.toLowerCase(), controller);        
     });
+    
+    // catch 404 and forward to error handler
+    app.use(function(req, res, next) {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    });
 };
 
 module.exports = registerRoutes;
