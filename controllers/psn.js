3c2997b2f2c4e7178ee3a46ff6cf11bacba262a3
Richard <richard.herman@gmail.com>
2015-09-29 21:25:17 +0100

    Begun structuring models.

--- controllers/psn.js
+++ controllers/psn.js
@@ -1,4 +1,4 @@
-var bungieApiService = require('../lib/bungieApiService'), 
+var bungieApiService = require('../lib/bungieService'), 
     express = require('express'),
     router = express.Router();
 
@@ -6,8 +6,8 @@ var bungieApiService = require('../lib/bungieApiService'),
 router.get('/:accountId/:characterId', function(req, res, next) {
     // test: http://localhost:3000/psn/4611686018437908853/2305843009230541270
     
-    bungieApiService.getCharacter(2, req.params.accountId, req.params.characterId, function(err, data) {
-        res.json(data);
+    bungieApiService.getCharacter(2, req.params.accountId, req.params.characterId, function(err, char) {
+        res.json(char);
     });
 });
 
