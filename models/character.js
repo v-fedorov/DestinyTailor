db8bb4cb9b385721d5a298a0be54a011488b35e6
Richard Herman <richard.herman@gmail.com>
2015-09-30 17:05:17 +0100

    Continued work on parsing items.

--- models/character.js
+++ models/character.js
@@ -3,21 +3,13 @@
  * @constructor
  */
 var Character = function() {
-    this.helmet = null;
-    this.gloves = null;
+    this.arms = null;
     this.chest = null;
+    this.helmet = null;
     this.legs = null;
     this.classItem = null;
     this.artifact = null;
     this.ghost = null;
-};
-
-/**
- * Sets the item on the character, for it's given item type.
- * @param {object} item The item to set.
- */
-Character.prototype.setItem = function(item) {
-    
 };
 
 module.exports = Character;
