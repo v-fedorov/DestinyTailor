1ceea8bd1cd1410fdc7ddcbfc8680bfc86f0c8e8
Richard Herman <richard.herman@gmail.com>
2015-10-05 17:01:57 +0100

    Beginning of iInventory loading

--- controllers/psn.js
+++ controllers/psn.js
@@ -1,4 +1,4 @@
-var bungieApiService = require('../lib/bungieService'),
+var bungieService = require('../lib/bungieService'),
     Character = require('../models/character'), 
     express = require('express'),
     router = express.Router();
@@ -8,21 +8,21 @@ router.get('/:accountId/:characterId', function(req, res, next) {
     // test: http://localhost:3000/psn/4611686018437908853/2305843009230541270
     
     var character = new Character(2, req.params.accountId, req.params.characterId);
-    /*bungieApiService.getCharacter(character, function(err, char) {
+    bungieService.getCharacter(character, function(err, char) {
         res.json(char);
-    });*/
+    });
     
-    /*bungieApiService.getInventoryItem(character, '6917529062061291933', function(err, item) {
+    /*bungieService.getInventoryItem(character, '6917529062061291933', function(err, item) {
        res.json(item); 
     });*/
     
-    bungieApiService.getInventory(character, function(err, result) {
+    /*bungieService.getInventory(character, function(err, result) {
         if (err) {
             res.status(err.code).send(err.message);
         } else {
             res.json(result);
         };
-    });
+    });*/
 });
 
 module.exports = router;
--- models/character.js
+++ models/character.js
@@ -3,17 +3,22 @@
  * @constructor
  */
 var Character = function(platform, accountId, id) {
+    // identifiers
     this.id = id;
     this.accountId = accountId;
     this.platform = platform;
     
-    this.arms = null;
+    // items
+    this.artifact = null;
     this.chest = null;
+    this.classItem = null;
+    this.gauntlets = null;
+    this.ghost = null;
+    this.heavyWeapon = null;
     this.helmet = null;
     this.legs = null;
-    this.classItem = null;
-    this.artifact = null;
-    this.ghost = null;
+    this.primaryWeapon = null;
+    this.specialWeapon = null;
 };
 
 module.exports = Character;
