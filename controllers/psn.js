1547c01acb206639cb3eb23d7df2ef2256e6cddb
Richard <richard.herman@gmail.com>
2015-10-05 20:22:35 +0100

    Fixed stack overflow exception with loading inventory.

--- controllers/psn.js
+++ controllers/psn.js
@@ -8,22 +8,23 @@ router.get('/:accountId/:characterId', function(req, res, next) {
     // test: http://localhost:3000/psn/4611686018437908853/2305843009230541270
     
     var bungie = new BungieService();
-    var character = new Character(2, req.params.accountId, req.params.characterId);
-    // bungie.getCharacter(character, function(err, char) {
-    //     res.json(char);
-    // });
     
-    bungie.getInventoryItem(character, '6917529062061291933', function(err, item) {
-       res.json(item); 
+    var character = new Character(2, req.params.accountId, req.params.characterId);
+    bungie.getCharacter(character, function(err, char) {
+        res.json(char);
     });
     
-    /*bungieService.getInventory(character, function(err, result) {
-        if (err) {
-            res.status(err.code).send(err.message);
-        } else {
-            res.json(result);
-        };
-    });*/
+    // bungie.getInventoryItem(character, '6917529062061291933', function(err, item) {
+    //    res.json(item); 
+    // });
+    
+    // bungie.getInventory(character, function(err, result) {
+    //     if (err) {
+    //         res.status(err.code).send(err.message);
+    //     } else {
+    //         res.json(result);
+    //     };
+    // });
 });
 
 module.exports = router;
