e1d5885aa56910e19df5373a66dd16f2621c870a
Richard Herman <richard.herman@gmail.com>
2015-10-12 15:37:29 +0100

    Added local config builder to grunt.

--- gruntfile.js
+++ gruntfile.js
@@ -1,7 +1,10 @@
 module.exports = function (grunt) {
+    var localConfigPath = 'server/config/local.json';
+
     // configure the tasks
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        local: grunt.file.exists(localConfigPath) ? grunt.file.readJSON(localConfigPath) : {},
         concat: {
             // concats and moves the required files from the bower components
             bower: {
@@ -28,14 +31,43 @@ module.exports = function (grunt) {
             bower: {
                 cmd: 'bower install'
             }
-        }
+        },
+        prompt: {
+            target: {
+                options: {
+                    questions: [
+                        {
+                            // API key
+                            name: "grunt-prompt.apiKey",
+                            message: 'Bungie API Key:',
+                            type: 'input',
+                            default: '<%= local.apiKey %>',
+                            validate: function(value) {
+                                return value !== undefined && value !== null && value !== '';
+                            }
+                        }
+                    ]
+                }
+            },
+        },
     });
 
     // load the tasks
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-exec');
+    grunt.loadNpmTasks('grunt-prompt');
+
+    // register the task to save the local config
+    grunt.registerTask('save-local-config', 'Saves the local configuration.', function() {
+        var config = {
+                apiKey: grunt.config('grunt-prompt.apiKey')
+            };
+
+        grunt.file.write(localConfigPath, JSON.stringify(config));
+        grunt.log.ok('Saved to [' + localConfigPath + ']');
+    });
 
     // register the available tasks
-    grunt.registerTask('default', ['exec:bower', 'concat:bower', 'copy:bower']);
+    grunt.registerTask('default', ['prompt:target', 'save-local-config', 'exec:bower', 'concat:bower', 'copy:bower']);
 };
