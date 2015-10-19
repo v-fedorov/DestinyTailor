b72758ca5f858696f2b8187b915756c784075b8d
GeekyEggo <richard.herman@gmail.com>
2015-10-19 21:31:09 +0100

    Added user service and character controller.

--- src/js/main.js
+++ src/js/main.js
@@ -1,38 +1,53 @@
 (function() {
     var app = angular.module('destinyTailorApp', []);
 
+    /**
+     * The user service, primarily used to share the current membership, and selected character.
+     */
     app.factory('userService', function() {
-        var account = null;
-        
-        return {
-            getAccount: function() {
-                return account;
-            },
-            setAccount: function(val) {
-                account = val;
-            }
-        };
+        return { membership: {} };
     });
-    
+
+    /**
+     * The search controller, primarily used to search for a user's account / membership with Bungie.
+     * @param {object} $scope The scope of the controller.
+     * @param {object} $http The http utils from Angular.
+     * @param {object} userService The user service.
+     */
     app.controller('searchController', ['$scope', '$http', 'userService', function ($scope, $http, userService) {
         var $platform = $('#platform');
-        $scope.error = '';
         
+        /**
+         * Searches for a character and updates the membership in the user service.
+         */
         $scope.search = function() {
             var path = "/api/" + ($platform[0].checked ? 2 : 1) + '/' + encodeURIComponent($scope.name) + '/';
             $scope.isLoading = true;
             
+            // begin the request
             $http.get(path).then(function(result) {
                 $scope.isLoading = false;
-                userService.setAccount(result.data);
-                
                 if (result.data === null) {
                     $scope.error = 'Unable to find character.';
+                } else {
+                    userService.membership = result.data;
                 };
             }, function(err) {
-                console.log(err);
                 $scope.error = err.statusText;
             });
         };
+    }]);
+    
+    /**
+     * Defines the membership controller, allowing the user to swap their selected character.
+     * @param {object} $scope The scope of the contorller.
+     * @param {object} userService The user service.
+     */
+    app.controller('characterController', ['$scope', 'userService', function($scope, userService) {
+        $scope.$watch(function() {
+            return userService.membership;
+        }, function(membership) {
+            $scope.membership = membership;
+        }, true)
     }]);
 })(); 
