178a1a9cf7e9645ba126225ba5cb1270127f262d
Richard Herman <richard.herman@gmail.com>
2015-11-04 16:08:32 +0000

    Added constant for ability cooldowns.

--- client/js/main.js
+++ client/js/main.js
@@ -2,9 +2,19 @@
     'use strict';
 
     var app = angular.module('destinyTailorApp', []);
+
+    // stat names
     app.constant('STAT_NAMES', [
-            'intellect',
-            'discipline',
-            'strength'
-        ]);
+        'intellect',
+        'discipline',
+        'strength'
+    ]);
+
+    // cooldown tiers for abilities
+    app.constants('ABILITY_COOLDOWNS', {
+        supersFast:  ['5:00', '4:46', '4:31', '4:15', '3:58', '3:40'], /* defender, nightstalker, striker, and sunsinger */
+        supersSlow:  ['5:30', '5:14', '4:57', '4:39', '4:20', '4:00'], /* bladedancer, gunslinger, stormcaller, sunbreaker, and voidwalker */
+        grendade:    ['1:00', '0:55', '0:49', '0:42', '0:34', '0:25'],
+        melee:       ['1:00', '0:55', '0:49', '0:42', '0:34', '0:25']
+    });
 })();
